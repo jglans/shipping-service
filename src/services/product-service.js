28dbcef7d7fdb7fbab7943db710eed163dbf4da9
Jonathan Glans <jonathan.glans@ikea.com>
2019-01-15 13:54:10 +0100

    microservice created (Lab2 done)

--- src/services/product-service.js
+++ src/services/product-service.js
@@ -3,10 +3,18 @@ var axios = require('axios')
 
 module.exports = {
   getProductWeight: async function (productId) {
+    let URL = process.env['MICROS_PRODUCTS_URL'] || 'mycluster.icp:8899/products';
     return axios
-      .get('https://mycluster.icp:8899/products/' + productId)
+      .get(`https://${URL}/${productId}`)
       .then(response => {
-        return response.data.weightLB
+        if (response.data && !Number.isNaN(parseFloat(response.data.weightLB))) {
+          return response.data.weightLB
+        } else {
+          return Promise.reject('Invalid response object')
+        }
+      })
+      .catch( (err) => {
+        throw new Error(err)
       })
   }
 }
