3cf2680a618d94043606d5a6c94a57f2f8167232
Jonathan Glans <jonathan.glans@ikea.com>
2019-01-15 12:46:58 +0100

    added product service

--- src/controllers/shipping-controller.js
+++ src/controllers/shipping-controller.js
@@ -1,8 +1,20 @@
 // src/controllers/shipping-controller.js
+var productService = require('../services/product-service')
+
 class ShippingController {
-  constructor() {}
-  getItemShipping(item) {
-    //return 0.5;
+
+  constructor() {
+  this.REGULAR_PRICE = 0.1
+  this.OVERNIGHT_PRICE = 1
+  }
+
+  async getItemShipping(item) {
+    var shippingAmount = await productService.getProductWeight(item.id)
+    if (item.type.toLowerCase() === 'overnight') {
+      return shippingAmount * this.OVERNIGHT_PRICE
+    } else {
+      return shippingAmount * this.REGULAR_PRICE
+    }
   }
 }
 
