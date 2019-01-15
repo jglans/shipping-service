76b4b84714927f4c9b81287f1c5bcbcacfa7973b
Rijad Sacirovic <rijad.sacirovic@rijads-mbp.mma.se.ibm.com>
2019-01-15 12:19:41 +0100

    added first test

---
+++ src/controllers/shipping-controller.js
@@ -0,0 +1,9 @@
+// src/controllers/shipping-controller.js
+class ShippingController {
+  constructor() {}
+  getItemShipping(item) {
+    //return 0.5;
+  }
+}
+
+module.exports = ShippingController;
